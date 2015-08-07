378b56e0c4430151648edf53f6a6664dd6d73e40
heskew-riotapps <hunter@riotapps.com>
2015-08-06 23:11:37 -0400

    initial

---
+++ Web/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            const string ANGULAR_APP_ROOT = "~/Client/scripts/app/";
+            const string VIRTUAL_BUNDLE_PATH = ANGULAR_APP_ROOT + "app.js";
+
+            var scriptBundle = new ScriptBundle(VIRTUAL_BUNDLE_PATH)
+                .Include(ANGULAR_APP_ROOT + "app.js")
+                .IncludeDirectory(ANGULAR_APP_ROOT, "*.js", searchSubdirectories: true);
+
+            bundles.Add(scriptBundle);
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+        }
+    }
+}
