b097d0d0b569f2d37fff9b73579f629e633f34b2
heskew-riotapps <hunter@riotapps.com>
2015-08-22 19:48:22 -0400

    changes that are not working

--- Web/Client/scripts/app/config/config.router.js
+++ Web/Client/scripts/app/config/config.router.js
@@ -9,36 +9,47 @@
  */
 angular.module('app')
   .run(
-    [           '$rootScope', '$state', '$stateParams',
-      function ( $rootScope,   $state,   $stateParams ) {
-        $rootScope.$state = $state;
-        $rootScope.$stateParams = $stateParams;
+    ['$rootScope', '$state', '$stateParams',
+      function ($rootScope, $state, $stateParams) {
+          $rootScope.$state = $state;
+          $rootScope.$stateParams = $stateParams;
       }
     ]
   )
   .config(
-    [          '$stateProvider', '$urlRouterProvider',
-      function ( $stateProvider,   $urlRouterProvider ) {
-        $urlRouterProvider
-          .otherwise('/app/dashboard');
-        $stateProvider
-          .state('app', {
-            abstract: true,
-            url: '/app',
-            views: {
-              '': {
-                  templateUrl: 'api/views/a/home/layout'
-              },
-              'aside': {
-                  templateUrl: 'api/views/a/home/asidenavuikit'
-              }
-            }
-          })
-            .state('app.dashboard', {
-              url: '/dashboard',
-              templateUrl: 'api/views/a/home/dashboard'
-             
+    ['$stateProvider', '$urlRouterProvider',
+      function ($stateProvider, $urlRouterProvider) {
+          $urlRouterProvider
+            .otherwise('/app/dashboard');
+          $stateProvider
+            .state('app', {
+                abstract: true,
+                url: '/app',
+                views: {
+                    '': {
+                        templateUrl: 'api/views/a/home/layout'
+                    },
+                    'aside': {
+                        templateUrl: 'api/views/a/home/asidenavuikit'
+                    }
+                }
             })
+          .state('main.kickstarter', {
+              url: '/kickstarter',
+              templateUrl: 'api/views/a/app/kickstarter'
+
+          })
+            .state('main.gameplan', {
+                url: '/gameplan',
+                templateUrl: 'api/views/a/app/gameplan'
+
+            })
+            .state('main.winvault', {
+                url: '/winvault',
+                templateUrl: 'api/views/a/app/winvault'
+
+            })
+             
 
       }
     ]
