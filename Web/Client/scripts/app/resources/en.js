d89fc17643c7f1cd6e80cbe54479059f3fdffa23
heskew-riotapps <hunter@riotapps.com>
2015-08-22 23:05:35 -0400

    Revert "font awesome"
    This reverts commit f1ef60a29c03bba5c307e2c9a4bb14e2dc5b3053.

--- Web/Client/scripts/app/resources/en.js
+++ Web/Client/scripts/app/resources/en.js
@@ -1,11 +1,11 @@
 'use strict';
 var enTranslations = {
-    "Header": {
-        "AppName": "RampedUp.io",
-        "NavBar": {
-            "Kickstarter": "Kickstarter",
-            "GamePlan": "Game Plan",
-            "WinVault": "Win Vault"
+    "header": {
+        "navbar": {
+            "DASHBOARD": "Dashboard",
+            "EMAIL": "Email",
+            "UIKITS": "UI Kits",
+            "PAGES": "Pages"
         }
     }
 }
