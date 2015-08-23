4c8ebbc268490a557085a189cfd9b6152b3affbd
heskew-riotapps <hunter@riotapps.com>
2015-08-22 23:04:54 -0400

    Revert "merge"
    This reverts commit 4c34ada186c46eba3babda2f16179f1b5720c02b.

--- Web/Client/scripts/app/resources/en.js
+++ Web/Client/scripts/app/resources/en.js
@@ -1,3 +1,5 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 'use strict';
 var enTranslations = {
     "Header": {
@@ -8,4 +10,22 @@ var enTranslations = {
             "WinVault": "Win Vault"
         }
     }
+=======
+=======
+>>>>>>> parent of f589cfb... setup
+{
+
+"header" : {
+  "navbar" : {
+    "DASHBOARD" : "Dashboard",
+    "EMAIL" : "Email",
+    "UIKITS" : "UI Kits",
+    "PAGES" : "Pages"
+  }
+}
+
+<<<<<<< HEAD
+>>>>>>> parent of f589cfb... setup
+=======
+>>>>>>> parent of f589cfb... setup
 }
