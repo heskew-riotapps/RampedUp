4c34ada186c46eba3babda2f16179f1b5720c02b
heskew-riotapps <hunter@riotapps.com>
2015-08-22 22:52:01 -0400

    merge

--- Web/Client/scripts/app/resources/en.js
+++ Web/Client/scripts/app/resources/en.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 'use strict';
 var enTranslations = {
     "Header": {
@@ -10,22 +8,4 @@ var enTranslations = {
             "WinVault": "Win Vault"
         }
     }
-=======
-=======
->>>>>>> parent of f589cfb... setup
-{
-
-"header" : {
-  "navbar" : {
-    "DASHBOARD" : "Dashboard",
-    "EMAIL" : "Email",
-    "UIKITS" : "UI Kits",
-    "PAGES" : "Pages"
-  }
-}
-
-<<<<<<< HEAD
->>>>>>> parent of f589cfb... setup
-=======
->>>>>>> parent of f589cfb... setup
 }
